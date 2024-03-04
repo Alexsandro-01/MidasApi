6ba435324a15be3d013d80ac84ba46653f4b3b79
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2024-03-03 21:55:38 -0300

    feat: Adiciona o Balance service no Program.cs
    Para que assim a injeção de dependência funcione no Balance Controller

--- MidasApi/Program.cs
+++ MidasApi/Program.cs
@@ -1,3 +1,6 @@
+using MidasApi.Services;
+using MidasApi.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Add services
+builder.Services.AddScoped<IBalanceService, BalanceService>();
 
 var app = builder.Build();
 
