cbc7df513584b360cbd2a12e0ae27e227ce4f034
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2024-03-05 20:20:27 -0300

    feat: Adiciona annotations aos atributos da classe

--- MidasApi/Models/Transaction.cs
+++ MidasApi/Models/Transaction.cs
@@ -1,10 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace MidasApi.Models;
 public class Transaction
 {
+  [Key]
+  public int Id {get; set;}
+  [Column(TypeName = "varchar(50)")]
   public string Type {get; set;}
   public double Value {get; set;}
   public DateTime Date {get; set;}
+  [Column(TypeName = "varchar(100)")]
   public string Description {get; set;}
+  [Column(TypeName = "varchar(100)")]
   public string TransactionId {get; set;}
 
   public Transaction(
@@ -21,4 +29,6 @@ public class Transaction
     Description = description;
     TransactionId = transactionId;
   }
+
+  public Transaction() {}
 }
