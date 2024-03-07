bb4c21cf603336e1540f19bdff3a7036f86e2f21
Alexsandro Pinheiro <alerrandrofire@gmail.com>
2024-03-07 19:44:15 -0300

    refactor: Altera o retorno do método ReadFile para TransactionDto

--- MidasApi/Interfaces/IBalanceService.cs
+++ MidasApi/Interfaces/IBalanceService.cs
@@ -1,10 +1,11 @@
 namespace MidasApi.Interfaces;
 
+using MidasApi.DTO;
 using MidasApi.Models;
 
 public interface IBalanceService
 {
   public void Create(IFormFile formFile);
   public string WriteFile(IFormFile formFile);
-  public List<Transaction> ReadFile(string filePath);
+  public TransactionsDto ReadFile(string filePath);
 }
